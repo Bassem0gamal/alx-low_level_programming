ca8733a903d3e28cd717ff92ecf5bc55fb3bd326
Basem Gamal <bassemgamalosman12@gmail.com>
2023-09-08 19:59:19 -0700

    test

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
-
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
-
+  *main - prints two pairs of two-digit numbers
+  *Return: 0
+*/
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+    int i, j;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+    for (i = 0; i <= 99; i++)
+        {
+            for (j = i + 1; j <= 99; j++)
+            {
+                putchar((i / 10) + '0');
+                putchar((i % 10) + '0');
+                putchar(' ');
+                putchar((j / 10) + '0');
+                putchar((j % 10) + '0');
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+                if (i < 98 || j < 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+            putchar('\n');
+            return (0);
 }
