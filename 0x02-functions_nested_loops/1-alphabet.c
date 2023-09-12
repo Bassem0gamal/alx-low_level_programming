33777ef1f87ea63fc4fd9667fff65e516ee0fe6e
Basem Gamal <bassemgamalosman12@gmail.com>
2023-09-12 13:33:47 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet
+ */
+
+void print_alphabet(void)
+{
+  char alphabet;
+
+  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    _putchar(letter);
+
+  _putchar('\n');
+}
