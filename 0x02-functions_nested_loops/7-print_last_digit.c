a7b679cdd7c80a960852b82c49c788da4f92c8b9
Basem Gamal <bassemgamalosman12@gmail.com>
2023-09-13 05:32:06 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ *print_last_digit - prints the last digit of a number
+ *@x: the int to get the last digit from
+ *Return:value for the last digit
+ */
+
+int print_last_digit(int x)
+{
+	int x;
+
+	_putchar(x % 10);
+}
+
