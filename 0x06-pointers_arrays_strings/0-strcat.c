f67b317515ae6b07a7d478f45583d73eccc06cf4
Basem Gamal <bassemgamalosman12@gmail.com>
2023-09-21 04:24:54 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include main.h
+
+/**
+ * char *_stract - concatenates two strings
+ * @*dest: first string
+ * @*src: second string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+  char dest[];
+  char src[];
+
+  strcat(dest, src);
+
+  return (0);
+}
