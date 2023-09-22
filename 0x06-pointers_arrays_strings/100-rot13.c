e6e0c77dd1c52c9078f809ad3990e4b7eca19c26
Basem Gamal <bassemgamalosman12@gmail.com>
2023-09-21 18:23:44 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * leet - encodes a string in 1337
+ * rot13 - encodes a string in rot13
  * @s: string to be encoded
  *
- * Return: the resulting string;
+ * Return: the resulting string
  */
-char *leet(char *s)
+char *rot13(char *s)
 {
 	int i, j;
 
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
+				break;
 			}
 		}
 	}
