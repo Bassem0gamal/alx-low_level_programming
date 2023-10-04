3b0076ac8b1fc27b53f69c79273ef3dbff2e288f
Basem Gamal <bassemgamalosman12@gmail.com>
2023-10-04 12:01:28 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,5 +12,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i = 0;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	p = (char *)malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
 }
